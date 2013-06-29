9b785d38c77515c65546db33d5e3a62732fbbd48
Jacob Chappell <chappellind@gmail.com>
2013-06-28 21:16:32 -0500

    Add load and unload functions for graphs.

--- controller.js
+++ controller.js
@@ -2,38 +2,78 @@
 	$( document ).ready(
 		function() {
 			var $collection = $( '.collection' );
-			var $graph = $( '.graph.active', $collection );
 			
-			var $nodes = $( '.node', $graph );
-			var $siblings = $nodes.filter( ':not(.origin)' );
-			
-			var radius_min = 100;
-			var radius_max = 150;
-			var angle = 0;
-			var distribution = 2 * Math.PI / $siblings.length;
-			
-			$siblings.each(
-				function() {
-					var radius = Math.floor( Math.random() * ( radius_max - radius_min ) + radius_min );
-					var x = radius * Math.cos( angle );
-					var y = radius * Math.sin( angle );					
-					
-					$( this ).css(
-						{
-							marginLeft: '+=' + x,
-							marginTop: '+=' + y
+			var unloadGraph = function( $graph, complete ) {
+				$( '.node', $graph ).css(
+					{
+						marginLeft: '-8px',
+						marginTop: '-8px'
+					}
+				);
+				
+				$graph
+					.off(
+						'.sunsoft',
+						'**'
+					)
+					.fadeOut(
+						250,
+						function() {
+							complete.call( this );
 						}
 					);
-					
-					angle += distribution;
-				}
-			);
+			};
 			
-			$( 'a', $nodes ).click(
-				function( e ) {
-					e.preventDefault();
-				}
-			);
+			var loadGraph = function( $graph ) {
+				var $nodes = $( '.node', $graph );
+				var $siblings = $nodes.filter( ':not(.origin)' );
+				
+				var radius_min = 100;
+				var radius_max = 150;
+				var angle = 0;
+				var distribution = 2 * Math.PI / $siblings.length;
+				
+				$graph.fadeIn(
+					250,
+					function() {
+						$( this ).on(
+							'click.sunsoft',
+							'.node a',
+							function( e ) {
+								e.preventDefault();
+								
+								var _self = this;
+								
+								unloadGraph(
+									$graph,
+									function() {
+										loadGraph( $( $( _self ).attr( 'href' ) ) );
+									}
+								);
+							}
+						);
+					}
+				);
+				
+				$siblings.each(
+					function() {
+						var radius = Math.floor( Math.random() * ( radius_max - radius_min ) + radius_min );
+						var x = radius * Math.cos( angle );
+						var y = radius * Math.sin( angle );					
+						
+						$( this ).css(
+							{
+								marginLeft: ( x - 8 ) + 'px',
+								marginTop: ( y - 8 ) + 'px'
+							}
+						);
+						
+						angle += distribution;
+					}
+				);
+			};
+			
+			loadGraph( $( '#home', $collection ) );
 		}
 	);
 })( jQuery );
